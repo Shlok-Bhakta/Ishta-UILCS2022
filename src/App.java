4725d566ae910cbff86c3fb4a8d7179ce554945a
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-02-16 13:55:44 -0600

    master

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+import java.io.File;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(new File("ishita.dat"));
+        int loop = scanner.nextInt();
+        //joel
         
+        for (int i = 0; i < loop; i++) {
+            String line = scanner.next();
+            //shlodk bads
+            
+
+
+        }
+
+
+
     }
 }
