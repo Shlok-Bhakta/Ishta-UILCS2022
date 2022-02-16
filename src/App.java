18a3e095e97fa80808bd1263bb27b61b50a21ff4
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-02-16 14:39:12 -0600

    2:38PM 2/16/22

--- src/App.java
+++ src/App.java
@@ -6,12 +6,33 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(new File("ishita.dat"));
         int loop = scanner.nextInt();
-        //joel
+
         
         for (int i = 0; i < loop; i++) {
             String line = scanner.next();
-            //shlodk bads
-            
+
+            int leftP = 0;
+            int rightP = 0;
+
+            for(int cursor = 0; cursor < line.length(); cursor++) {
+                
+                if(line.toCharArray()[cursor] == '(') {
+                leftP++;   
+                }
+                if(line.toCharArray()[cursor] == ')'){
+                leftP--;
+                    rightP++;
+                }
+
+
+            }
+            if(leftP == rightP){
+                System.out.println(--leftP);
+                
+            }else{
+                System.out.println(0);
+            }
+
 
 
         }
@@ -19,4 +40,28 @@ public class App {
 
 
     }
-}
+
+    public static int combos(char[] paren){
+        int com = 0;
+        
+        for(int t = 0; t < paren.length; t++){
+            if (paren[t] == '(') {
+                
+                if(paren[t] == ')'){
+
+                }else{
+                    combos(paren);
+                }
+                
+            }else{
+                com = 0;
+            }
+
+        }
+
+        
+        return com;
+
+
+    }
+    }
