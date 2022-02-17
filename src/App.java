7e04941c8b285ed414da9cfbedc4084d74217a1f
Shlok-Bhakta <shlokbhakta1@gmail.com>
2022-02-17 16:25:52 +0000

    10:L25 2/25/21

--- src/App.java
+++ src/App.java
@@ -3,24 +3,52 @@ import java.io.File;
 
 
 public class App {
-    public static int position = 0;
-    public static int comb = 0;
+
+    public static int linespaces = 0;
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(new File("ishita.dat"));
         int loop = scanner.nextInt();
 
         
         for (int i = 0; i < loop; i++) {
+            linespaces = 0;
             String line = scanner.next();
 
+            for (int j = 0; j < line.length(); j++){
+                for(int k = 0; k < line.length(); k++){
+                    if(line.charAt(k) == '('){ 
+                        if(line.charAt(k + 1) == ')'){
+                            linespaces++;
+                        }else{
+                            k++;
+                        }
+                    }
 
-            
+                }
+                
+            }
+    
+            System.out.println(linespaces);
+
 
 
         }
 
 
 
+    }
+    public static void numspaces(char input, char nextin){
+        if(input == '('){
+            if(nextin == ')'){ 
+                linespaces++;
+            }
+
+        }
+
+
+
+        
+        
     }
 
     }
